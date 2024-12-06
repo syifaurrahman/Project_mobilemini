15d54ae9d02c04f24b1e2c151cc768254d623a52
rahmatisma <rahmat158jkt@gmail.com>
2024-12-06 13:54:56 +0700

    Tes commit rahmat

--- food_delivery_app/lib/main.dart
+++ food_delivery_app/lib/main.dart
@@ -118,7 +118,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.book),
+        child: const Icon(Icons.home),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
