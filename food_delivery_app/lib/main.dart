024f9c4e7b5103434daea098e4850fa3dcb6303c
vivijumilia <vivijumilia7@gmail.com>
2024-12-06 15:01:26 +0700

    test

--- food_delivery_app/lib/main.dart
+++ food_delivery_app/lib/main.dart
@@ -118,7 +118,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.close),
+        child: const Icon(Icons.home_filled),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
