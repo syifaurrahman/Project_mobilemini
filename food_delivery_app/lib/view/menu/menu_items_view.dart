638f527d33bb42579ed86deef2c9befa0bbb0619
Syifaurrahman <siffstayganz@gmail.com>
2024-12-13 16:07:10 +0700

    menu items view

--- food_delivery_app/lib/view/menu/menu_items_view.dart
+++ food_delivery_app/lib/view/menu/menu_items_view.dart
@@ -3,14 +3,10 @@ import 'package:food_delivery_app/common/color_extension.dart';
 import 'package:food_delivery_app/common_widget/round_textfield.dart';
 
 import '../../common_widget/category_cell.dart';
-import '../../common_widget/most_popular_cell.dart';
-import '../../common_widget/popular_resutaurant_row.dart';
-import '../../common_widget/recent_item_row.dart';
-import '../../common_widget/view_all_title_row.dart';
 
 class MenuItemsView extends StatefulWidget {
   final Map mObj;
-  const MenuItemsView({super.key});
+  const MenuItemsView({super.key, required this.mObj});
 
   @override
   State<MenuItemsView> createState() => _MenuItemsViewState();
@@ -19,30 +15,36 @@ class MenuItemsView extends StatefulWidget {
 class _MenuItemsViewState extends State<MenuItemsView> {
   TextEditingController txtSearch = TextEditingController();
 
-  List popArr = [
+  List menuItemsArr = [
     {
-      "image": "assets/img/res_1.png",
-      "name": "Minute by tuk tuk",
+      "image": "assets/img/dess_1.png",
+      "name": "French apple pie",
       "rate": "4.9",
       "rating": "124",
-      "type": "Cafa",
-      "food_type": "Western Food"
+      "type": "minute by tuk tuk",
+      "food_type": "Desserts"
     },
     {
-      "image": "assets/img/res_2.png",
-      "name": "Café de Noir",
+      "image": "assets/img/dess_2.png",
+      "name": "Dark chocolate cake",
       "rate": "4.9",
       "rating": "124",
-      "type": "Cafa",
-      "food_type": "Western Food"
+      "type": "cakes by Tella",
+      "food_type": "Desserts"
     },
-    {
-      "image": "assets/img/res_3.png",
-      "name": "Bakes by Tella",
+    {"image": "assets/img/dess_3.png",
+      "name": "Street shake",
       "rate": "4.9",
       "rating": "124",
-      "type": "Cafa",
-      "food_type": "Western Food"
+      "type": "Cafe Racer", 
+      "food_type": "Desserts"
+    },
+     {"image": "assets/img/dess_4.png",
+      "name": "Fudgy chewy Brownies",
+      "rate": "4.9",
+      "rating": "124",
+      "type": "minute by tuk tuk",
+      "food_type": "Desserts"
     },
   ];
 
@@ -65,8 +67,9 @@ class _MenuItemsViewState extends State<MenuItemsView> {
                         onPressed: () {},
                         icon: Image.asset("assets/img/btn_back.png",
                             width: 20, height: 20)),
+                         const SizedBox(width: 8,),
                     Text(
-                      "Good morning Akila!",
+                      Widget.mObj["name"].toString(),
                       style: TextStyle(
                           color: TColor.primaryText,
                           fontSize: 20,
@@ -104,48 +107,6 @@ class _MenuItemsViewState extends State<MenuItemsView> {
                   );
                 }),
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: ViewAllTitleRow(
-                  title: "Most Popular",
-                  onView: () {},
-                ),
-              ),
-              SizedBox(
-                height: 200,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  itemCount: mostPopArr.length,
-                  itemBuilder: ((context, index) {
-                    var mObj = mostPopArr[index] as Map? ?? {};
-                    return MostPopularCell(
-                      mObj: mObj,
-                      onTap: () {},
-                    );
-                  }),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: ViewAllTitleRow(
-                  title: "Recent Items",
-                  onView: () {},
-                ),
-              ),
-              ListView.builder(
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                itemCount: recentArr.length,
-                itemBuilder: ((context, index) {
-                  var rObj = recentArr[index] as Map? ?? {};
-                  return RecentItemRow(
-                    rObj: rObj,
-                    onTap: () {},
-                  );
-                }),
-              )
             ],
           ),
         ),
